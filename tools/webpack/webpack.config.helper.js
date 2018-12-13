bc39f6095e9271984a740db63ec174cefe0928f9
djey47 <djey47@github.com>
2018-12-13 16:01:23 +0100

    chore(webpack): add helper bundle configuration

---
+++ tools/webpack/webpack.config.helper.js
@@ -0,0 +1,18 @@
+/** Configuration for server-side webpack bundle **/
+
+const { outputPath, commonRules, commonPlugins } = require('./webpack.config.common.js');
+
+module.exports = {
+  target: 'node',
+  entry: './src/server/helper.js',
+  output: {
+    filename: 'node_helper.js',
+    path: outputPath,
+    libraryTarget: 'umd',
+  },
+  externals: [ 'node_helper' ], // MM2 provided
+  module: { 
+    rules: commonRules.concat([]),
+  },
+  plugins: commonPlugins.concat([]),
+};
