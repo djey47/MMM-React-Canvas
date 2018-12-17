730f9e735631133f8d84fc5c58bddf1d08484ca6
djey47 <djey47@github.com>
2018-12-17 22:43:20 +0100

    chore(webpack): generate styles bundle

--- tools/webpack/webpack.config.module.js
+++ tools/webpack/webpack.config.module.js
@@ -5,7 +5,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const EditJsonFile = require('edit-json-file');
 const appRootPath = require('app-root-dir').get();
 
-const isDevMode = process.env.NODE_ENV !== 'production';
 const packageFile = EditJsonFile(`${appRootPath}/package.json`);
 
 module.exports = {
@@ -19,18 +18,18 @@ module.exports = {
       test: /\.(css|scss)$/,
       exclude: /(node_modules)/,
       use: [
-        isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        MiniCssExtractPlugin.loader,
         'css-loader',
         'sass-loader',
-      ],      
+      ],
     }]),
   },
   plugins: commonPlugins.concat([
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: isDevMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isDevMode ? '[id].css' : '[id].[hash].css',
+      filename: 'styles.css',
+      chunkFilename: '[id].css',
     }),
   ]),
   node: {
